27661a5edc551b002a78ba680b2d3beeab7c3382
aayus karki <karki.aayush2003@icloud.com>
2026-01-14 17:08:08 +0545

    feat: add business logic for user creation

---
+++ server/src/api/user/userService.ts
@@ -0,0 +1,33 @@
+import { StatusCodes } from "http-status-codes";
+import { User, type CreateUser } from "./userModel";
+import { UserRepository } from "./userRepository";
+import { ConflictError } from "@/common/utils/customError";
+import { ServiceResponse } from "@/common/utils/serviceResponse";
+import logger from "@/common/utils/logger";
+
+class UserService {
+    private userRepository: UserRepository;
+
+    constructor(userRepository: UserRepository = new UserRepository()) {
+        this.userRepository = userRepository;
+    }
+
+    async createUser(user: CreateUser): Promise<ServiceResponse<User | null>> {
+        try {
+            const usernameExists = await this.userRepository.usernameExists(user.username);
+            if (usernameExists) {
+                throw new ConflictError("Username already exists");
+            }
+            const createdUser = await this.userRepository.createUser(user.username);
+            return ServiceResponse.success<User>("User created successfully", createdUser, StatusCodes.CREATED);
+        } catch (error) {
+            logger.error("Error creating user:", error);
+            if (error instanceof ConflictError) {
+                return ServiceResponse.failure(error.message, null, error.statusCode);
+            }
+            return ServiceResponse.failure("Failed to create user", null, StatusCodes.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
+
+export const userService = new UserService();
