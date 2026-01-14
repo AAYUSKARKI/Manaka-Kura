38e93a82ad9657e0df2195600164fe19d7bb6d8c
aayus karki <karki.aayush2003@icloud.com>
2026-01-14 17:06:47 +0545

    feat: add api for user creation

---
+++ server/src/api/user/userController.ts
@@ -0,0 +1,13 @@
+import { Request, RequestHandler, Response } from "express";
+import { CreateUserSchema, User } from "./userModel";
+import { handleServiceResponse, ServiceResponse } from "@/common/utils/serviceResponse";
+import { userService } from "./userService";
+class UserController {
+    public createUser: RequestHandler = async (req: Request, res: Response) => {
+        const data = CreateUserSchema.parse(req.body);
+        const serviceResponse: ServiceResponse<User | null> = await userService.createUser(data);
+        return handleServiceResponse(serviceResponse, res);
+    }
+}
+
+export const userController = new UserController();
