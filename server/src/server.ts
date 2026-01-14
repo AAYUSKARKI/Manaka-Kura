677ea3ca464eb63ac38bf795aa98342eedc89789
aayus karki <karki.aayush2003@icloud.com>
2026-01-14 17:07:44 +0545

    feat: add socket server and integrate user routes into main app

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,9 +4,19 @@ import helmet from "helmet";
 import http from "http";
 import rateLimiter from "./common/middleware/rateLimiter";
 import errorHandler from "./common/middleware/errorHandler";
+import { Server } from "socket.io";
+import { userRouter } from "./api/user/userRouter";
+import { openAPIRouter } from "./api-docs/openAPIRouter";
 
 const app: Express = express();
 const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: process.env.CORS_ORIGIN,
+        credentials: true
+    }
+});
 
 // Middlewares
 app.use(helmet());
@@ -17,6 +27,11 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", userRouter);
+
+app.use(openAPIRouter);
+
 app.use(errorHandler());
 
-export { server };
+export { server, io };
