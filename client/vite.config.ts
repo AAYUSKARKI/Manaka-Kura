df89e43ef696c13768343286a78b02f348d0efe3
aayus karki <karki.aayush2003@icloud.com>
2026-01-18 15:06:26 +0545

    feat: add PWA support with VitePWA configuration and manifest details

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,12 +2,30 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      strategies: 'injectManifest', 
+      srcDir: 'src',
+      filename: 'sw.ts', 
+      manifest: {
+        name: 'Manaka Kura',
+        short_name: 'ManakaKura',
+        description: 'Seamless connections, Infinite Conversations At Manaka Kura',
+        theme_color: '#000000', 
+        icons: [
+          { src: 'image.png', sizes: '192x192', type: 'image/png' },
+          { src: 'image.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
